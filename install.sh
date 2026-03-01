6229b4ac51e77a8bca1e4de34334a9df81cc0918
sogo <sogonishi@gmail.com>
2026-03-01 15:20:07 -0500

    add vim config

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 CONFIGS="$HOME/dotfiles/configs"
 
+# vim
+ln -sf "$CONFIGS/vim/vimrc" "$HOME/.vimrc"
+echo "linked: vim config"
+
 # nvim
 mkdir -p "$HOME/.config"
 if [ -e "$HOME/.config/nvim" ] && [ ! -L "$HOME/.config/nvim" ]; then
   mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak"
 fi
 ln -sf "$CONFIGS/nvim" "$HOME/.config/nvim"
+echo "linked: nvim config"
